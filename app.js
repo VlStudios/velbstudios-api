80e97ad8c83efc0c0e83a5229655a535ee901cfc
VlStudios <larinho2020@gmail.com>
2021-09-11 08:52:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
+const rotaUsuarios = require('./routes/usuarios');
+
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());   
+
+app.use(( req, res, next) => {
+    res.header('Acces-Control-Allow-Origin', '*');
+    res.header(
+        'Acces-Control-Allow-Header',
+        'Origin, X-Requrested-With, Content-Type, Accept, Authorization'
+    );
+
+    if(req.method === 'OPITIONS') {
+        res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+});
+
+app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos);
+app.use('/usuarios', rotaUsuarios);
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontado');
+    erro.status=404;
+    next(erro);
+ });
+
+ app.use((error, req, res, next) => {
+     res.status(error.status || 500);
+     return res.send({
+         erro: {
+             mensagem: error.message
+         }
+     });
+ });
+
+module.exports = app;
---
+++ routes/produtos.js
@@ -0,0 +1,157 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+
+router.get('/' , (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM produtos;',
+            (error , result, field) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                const response = {
+                    quantdade: result.length,
+                    produtos: result.map(prod => {
+                        return {
+                            id_produto: prod.id_produto,
+                            nome: prod.nome,
+                            preco: prod.preco,
+                            request: {
+                                tipo: 'GET',
+                                descrição: 'retorna os detalhes de um produto específico',
+                                url: 'http://localhost:3000/produtos/' + prod.id_produto
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send(response);
+            }
+        )
+    });
+});
+
+router.post('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?,?)',
+            [req.body.nome, req.body.preco],
+            (error, result, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error }) }
+                const response = {
+                    mensagem : 'Produto inserido com sucesso',
+                    ProdutoCriado: {
+                        id_produto: result.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'GET',
+                            descrição: 'Retorna todos os produtos ',
+                            url: 'http://localhost:3000/produtos/'
+                        }
+                    }
+                }
+                return res.status(201).send(response);
+            }
+        )
+    });
+});
+
+router.get('/:id_produto', (req, res, next)=> {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produto = ?;',
+            [req.params.id_produto],
+            (error, result, field) => {
+                if (error) { return res.status(500).send({ error: error }) }
+
+                if (result.length == 0) {
+                    return res.status(404).send({
+                        mensagem: 'Não foi encontrado o produto com esse ID'
+                    })
+                }
+                const response = {
+                    produto: {
+                        id_produto: result[0].id_produto,
+                        nome: result[0].nome,
+                        preco: result[0].preco,
+                        request: {
+                            tipo: 'GET',
+                            descrição: 'Retorna todos os produtos ',
+                            url: 'http://localhost:3000/produtos/'
+                        }
+                    }
+                }
+                return res.status(200).send(response);
+            }
+        )
+    });
+});
+ 
+router.patch('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `UPDATE produtos
+                SET nome       =?,
+                    preco      =?
+            WHERE id_produto   =?`,
+                [
+                    req.body.nome,
+                    req.body.preco,
+                    req.body.id_produto
+                ],
+                (error, resultado, field) => {
+                    conn.release();
+                    if (error) { return res.status(500).send({ error: error }) }
+                const response = {
+                    mensagem : 'Produto atualizado com sucesso',
+                    produtoAtualizado: {
+                        id_produto: req.body.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'GET',
+                            descrição: 'retorna os detalhes de um produto específico',
+                            url: 'http://localhost:3000/produtos/' + req.body.id_produto
+                        }
+                    }
+                }
+                return res.status(202).send(response);
+            }
+        )
+    });
+});
+
+router.delete('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `DELETE FROM produto WHERE id_produto = ?`,[req.body.id_produto],
+            (error, result, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error }) }
+                const response = {
+                    mensagem: 'Produto removido com sucesso',
+                    request: {
+                        tipo: 'POST',
+                        descricao: 'Insere um produto',
+                        url: 'http://localhost:3000/produtos',
+                        body: {
+                            nome: 'String',
+                            preco: 'Number'
+                        }
+
+                    }
+                }
+                return res.status(202).send(response);
+            }
+        )
+    });
+});
+
+module.exports = router;
+
+
